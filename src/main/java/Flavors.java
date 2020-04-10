e77a5112c71c27b338e923dcc288fd94132e0943
Marc Gomez <marc.gomez@olmero.ch>
2020-04-09 19:13:53 -0600

    Improved Flavors challenge solution

--- src/main/java/Flavors.java
+++ src/main/java/Flavors.java
@@ -1,53 +1,40 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static java.util.Comparator.comparingInt;
 
 /**
  * From https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem
  */
 public class Flavors {
 
-    static class CostItem {
-        private int cost;
-        private int index;
-
-        public CostItem(int cost, int index) {
-            this.cost = cost;
-            this.index = index;
-        }
-    }
 
     // Complete the whatFlavors function below.
     static String whatFlavors(int[] cost, int money) {
-        CostItem[] costItems = IntStream.range(0, cost.length)
-                .mapToObj(i -> new CostItem(cost[i], i+1))
-                .filter(c -> c.cost < money)
-                .sorted(comparingInt(c -> c.cost))
-                .toArray(CostItem[]::new);
-                //.collect(Collectors.toList());
+        int[] scost = Arrays.stream(cost).sorted().toArray();
 
+        for (int index1 = 0; index1 < cost.length; index1++) {
+            int costItem1 = cost[index1];
+            int costItem2 = money - costItem1;
 
-        for (int i = 0; i < costItems.length; i++) {
-            for (int j = i + 1; j < costItems.length; j++) {
-                int sum = costItems[i].cost + costItems[j].cost;
-                if (sum == money) {
-                    int[] indexes = {costItems[i].index, costItems[j].index};
-                    int[] sortedIndexes = Arrays.stream(indexes).sorted().toArray();
-                    System.out.println(sortedIndexes[0] + " " + sortedIndexes[1]);
-                    return sortedIndexes[0] + " " + sortedIndexes[1];
-                } else if (sum > money) {
-                    break;
+            if (Arrays.binarySearch(scost, costItem2) >= 0) {
+                int index2 = indexOf(cost, costItem2, index1);
+                if (index2 > 0) {
+                    int first = Math.min(index1, index2) + 1;
+                    int second = Math.max(index1, index2) + 1;
+                    return first + " " + second;
                 }
             }
-
         }
 
         return null;
+    }
+
+    static int indexOf(int[] input, int element, int indexToSkip) {
+        for (int i = 0; i < input.length; i++) {
+            if (i != indexToSkip && input[i] == element) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
--- src/test/java/FlavorsTest.java
+++ src/test/java/FlavorsTest.java
@@ -6,9 +6,15 @@ import static org.junit.Assert.*;
 
 public class FlavorsTest {
 
+
     @Test
-    public void flavors_test_case_2() {
+    public void flavors_test_case_2_1() {
         assertThat(Flavors.whatFlavors(new int[]{4, 3, 2, 5, 7}, 8)).isEqualTo("2 4");
+    }
+
+    @Test
+    public void flavors_test_case_2_2() {
         assertThat(Flavors.whatFlavors(new int[]{7, 2, 5, 4, 11}, 12)).isEqualTo("1 3");
     }
+
 }
