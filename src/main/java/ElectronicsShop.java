84c3dfde003bfa992024fb4630ca40179907218a
Marc Gomez <marc.gomez@olmero.ch>
2020-04-11 10:53:26 -0600

    Slightly improved version of Electronics Shop problem

--- src/main/java/ElectronicsShop.java
+++ src/main/java/ElectronicsShop.java
@@ -20,4 +20,25 @@ public class ElectronicsShop {
         return closestDif != Integer.MAX_VALUE ? b - closestDif : -1;
     }
 
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpentOptimized(int[] keyboards, int[] drives, int b) {
+        Arrays.sort(keyboards);
+        Arrays.sort(drives);
+
+        int closestDif = Integer.MAX_VALUE;
+        for (int i = 0; i < keyboards.length; i++) {
+            for (int j = drives.length - 1; j >= 0; j--) {
+                int dif = b - (keyboards[i] + drives[j]);
+                if (dif >= 0 && dif < closestDif) {
+                    closestDif = dif;
+                    break;
+                }
+            }
+        }
+
+        return closestDif != Integer.MAX_VALUE ? b - closestDif : -1;
+    }
+
 }
